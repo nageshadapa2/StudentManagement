051e1b86d566461001599c3293de448b7cd17fc6
Nagesh Adapa <nagesh.adapa@chandusoft.com>
2026-07-23 11:39:11 +0530

    Added StudentController

---
+++ StudentManagement.API/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagement.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            return Ok("welcome to student management system");
+        }
+
+    }
+}
