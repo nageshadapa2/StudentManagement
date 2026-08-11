12cc296254c93b3f8d7b0324997ba69c47bedde9
Nagesh Adapa <nageshadapa127@gmail.com>
2026-08-11 18:11:57 +0530

    Added EF Core entity relationships

---
+++ StudentManagement.API/Models/Department.cs
@@ -0,0 +1,6 @@
+﻿namespace StudentManagement.API.Models
+{
+    public class Department
+    {
+    }
+}
--- StudentManagement.API/Models/Student.cs
+++ StudentManagement.API/Models/Student.cs
@@ -9,6 +9,21 @@
         public int Age { get; set; }
 
         public string Email { get; set; } = string.Empty;
+        public StudentProfile? Profile { get; set; }
+        public Department Department { get; set; }
 
+
+
+    }
+
+    public class StudentProfile
+    {
+        public int Id { get; set; }
+
+        public string Address { get; set; } = string.Empty;
+
+        public int StudentId { get; set; }
+
+        public Student? Student { get; set; }
     }
 }
