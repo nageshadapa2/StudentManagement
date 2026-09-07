39922ee3eeade0baade5a65effdf8c4ce60c6f94
Nagesh Adapa <nageshadapa127@gmail.com>
2026-09-07 16:39:33 +0530

    Added global exception handling middleware

--- StudentManagement.API/Program.cs
+++ StudentManagement.API/Program.cs
@@ -31,8 +31,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseMiddleware<ExceptionMiddleware>();
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
